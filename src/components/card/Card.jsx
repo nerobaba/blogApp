aa819918779c86884be64eca063a15a0461f7b98
hakju <gkrwn0308@gmail.com>
2023-09-30 19:29:22 +0900

    1:21:25

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,23 +7,25 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill />
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023</span>
+          <span className={styles.date}>11.02.2023 - </span>
           <span className={styles.category}>CULTURE</span>
         </div>
         <Link href="/">
           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
         </Link>
-        <p>
+        <p className={styles.desc}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus
           dolor delectus eaque ducimus. Natus consectetur magnam eveniet
           laudantium quaerat voluptas itaque, error animi illo optio ullam
           corporis similique velit nobis.
         </p>
-        <Link href="/blog">Read More</Link>
+        <Link href="/blog" className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
